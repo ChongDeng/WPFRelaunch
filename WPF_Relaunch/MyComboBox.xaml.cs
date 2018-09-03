a280e9f8a970a7944d73a2b56124e7288705898a
chongdeng <fqyyang@163.com>
2018-09-03 10:16:14 -0700

    add data binding for combobox

--- WPF_Relaunch/MyComboBox.xaml.cs
+++ WPF_Relaunch/MyComboBox.xaml.cs
@@ -20,9 +20,23 @@ namespace WPF_Relaunch
     /// </summary>
     public partial class MyComboBox : UserControl
     {
+        public List<Student> StudentList { get; set; }
+
         public MyComboBox()
         {
             InitializeComponent();
+            this.StudentList = Student.GetStudents();
+            this.DataContext = this;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var cb = (ComboBox)sender;
+            var selectedItem = cb.SelectedItem as Student;
+            var selectedValue = cb.SelectedValue;
+            MessageBox.Show(string.Format("selectedItem={0} selectedValue={1}", 
+                selectedItem.Name, selectedValue));
+
         }
     }
 }
---
+++ WPF_Relaunch/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Relaunch
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Sex { get; set; }
+        public string Address { get; set; }
+
+        public static List<Student> GetStudents()
+        {
+            var studentList = new List<Student>();
+            for (int i = 0; i < 10; i++)
+            {
+                studentList.Add(new Student
+                {
+                    Name = string.Format("张 {0}", i + 1),
+                    Age = 10 + i,
+                    Sex = i % 2 == 0 ? "Boy" : "Girl",
+                    Address = string.Format("北京市朝阳区某某路{0}号", i + 1)
+                });
+            }
+            return studentList;
+        }
+    }
+}
