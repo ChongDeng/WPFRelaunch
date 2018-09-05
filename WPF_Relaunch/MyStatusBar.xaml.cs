1802abe2afb5c704fdc3f1ea73cee9c566dd6836
chongdeng <fqyyang@163.com>
2018-09-04 21:23:56 -0700

    added statusbar retest

---
+++ WPF_Relaunch/MyStatusBar.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Relaunch
+{
+    /// <summary>
+    /// Interaction logic for MyStatusBar.xaml
+    /// </summary>
+    public partial class MyStatusBar : UserControl
+    {
+        public MyStatusBar()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var tb = (TextBox)sender;
+            this.tblCount.Text = tb.Text.Length.ToString();
+        }
+
+    }
+}
